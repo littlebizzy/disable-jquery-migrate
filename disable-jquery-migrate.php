0bc51673557b29b4d6744378c6adae6c4fdcb6d2
Pau <pauiglesias@gmail.com>
2018-01-31 13:41:15 +0100

    Minor changes.

--- disable-jquery-migrate.php
+++ disable-jquery-migrate.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Disable jQuery Migrate
-Plugin URI: https://www.littlebizzy.com/plugins/delete-expired-transients
+Plugin URI: https://www.littlebizzy.com/plugins/disable-jquery-migrate
 Description: Easily prevent the jQuery migrate script that is included with WordPress core from being loaded to slim down source code (for advanced users only).
 Version: 1.0.0
 Author: LittleBizzy
